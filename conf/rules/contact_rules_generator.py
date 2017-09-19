a0bd548b6c12e7401bc6f76f0508003ae1f3b68d
Mykola Gerasymenko <nickoleg@gmail.com>
2017-09-19 20:31:44 +0000

    modified python script for rules_generator

--- conf/rules/contact_rules_generator.py
+++ conf/rules/contact_rules_generator.py
@@ -1,17 +1,18 @@
-template = '''rule "Switch Light LED{3} State Update"
+template = '''rule \"Switch Light LED{3} State Update\"
 when
     Item {0} received update
 
 then
-    if({0}.state == ON) {
+    if({0}.state == ON) {{
         {1}.state = OFF
         {2}.postUpdate(OPEN)
-    } else {
+    }} else {{
         {1}.state = ON
         {2}.postUpdate(CLOSED)
-    }
+    }}
 end'''
 
-str = template.format('Light_MCP23017Contact_B0','MCP23017Contact_B0_LED','MCP23017Contact_B0', 0)
-print(str)
-
+for i in range(0,8):
+    f = open('MCP23017Contact_B{}.rules'.format(i), 'w')
+    str = template.format('Light_MCP23017Contact_B{}'.format(i),'MCP23017Contact_B{}_LED'.format(i),'MCP23017Contact_B{}'.format(i), i)
+    f.write(str)
