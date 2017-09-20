a10a054b6603721cc03a94977b8434d77b7993b6
Mykola Gerasymenko <nickoleg@gmail.com>
2017-09-20 06:30:02 +0000

    AMEND, forgot to add promoted files;

--- conf/rules/contact_rules_generator.py
+++ conf/rules/contact_rules_generator.py
@@ -1,30 +1,27 @@
-template = '''rule \"Switch Light LED{3} State Update\"
+template = '''rule "Switch state of LED{2} Changed"
 when
     Item {0} received update
-
 then
-    if({0}.state == ON) {{
-        {1}.state = OFF
-        {2}.postUpdate(OPEN)
-    }} else {{
-        {1}.state = ON
-        {2}.postUpdate(CLOSED)
-    }}
+    if({0}.state == ON)
+        {1}.sendCommand(OFF)
+    else
+        {1}.sendCommand(ON)
 end'''
 
-template2 = '''rule \"Contact state of LED{2} Changed\"
+template2 = '''rule \"Contact state of LED{3} Changed\"
 when
     Item {0} changed
-
 then
-    if({0}.state == OPEN) {{
-        {1}.sendCommand(ON)
-    }} else {{
-        {1}.sendCommand(OFF)
-    }}
+    if({2}.state == ON)
+        {1}.postUpdate(ON)
+    else
+        {1}.postUpdate(OFF)
 end'''
 
 for i in range(0,8):
     f = open('MCP23017ContactB{}.rules'.format(i), 'w')
-    str = template2.format('MCP23017ContactB{}'.format(i),'MCP23017ContactB{}_LED'.format(i), i)
+    str = template.format('MCP23017VirtualSwitchB{}'.format(i),'MCP23017ContactB{}_LED'.format(i), i)
+    f.write(str)
+    f.write("\r\n \r\n")
+    str = template2.format('MCP23017ContactB{}'.format(i),'MCP23017VirtualSwitchB{}'.format(i),'MCP23017ContactB{}_LED'.format(i), i)
     f.write(str)
